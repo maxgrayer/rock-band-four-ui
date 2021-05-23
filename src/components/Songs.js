34fc40e72ed89fe3181ae995afd03fa3ef8c239a
Max Grayer <mgrayer@andplus.com>
2021-05-23 15:29:35 -0400

    Adds Axios for API request. Adds error handling.

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,27 +1,41 @@
 import React, { useEffect, useState } from "react";
 import SongsLoading from "./SongsLoading";
 import BareList from "./BareList";
+import axios from "axios";
 
 function Songs() {
   const BareListWithLoading = SongsLoading(BareList);
   const [appState, setAppState] = useState({
     loading: false,
+    errorMessage: null,
     songs: null,
   });
 
   useEffect(() => {
-    setAppState({ loading: true, songs: null });
+    setAppState({ loading: true, errorMessage: null, songs: null });
     const apiUrl = "http://localhost:8080/songs/";
-    fetch(apiUrl)
-      .then((response) => response.json())
+    axios
+      .get(apiUrl, {
+        timeout: 5000,
+      })
+      .then((response) => response.data)
       .then((songs) => {
-        setAppState({ loading: false, songs: songs });
+        setAppState({ loading: false, errorMessage: null, songs: songs });
+      })
+      .catch(function (error) {
+        console.log("FULL ERROR: " + error);
+        setAppState({
+          loading: false,
+          errorMessage: error.message,
+          songs: null,
+        });
       });
   }, [setAppState]);
   return (
     <div className="songs-container">
       <BareListWithLoading
         isLoading={appState.loading}
+        errorMessage={appState.errorMessage}
         songs={appState.songs}
       />
     </div>
--- src/components/SongsLoading.js
+++ src/components/SongsLoading.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Alert } from "react-bootstrap";
 
 function SongsLoading(Component) {
-  return function SongsLoadingComponent({ isLoading, ...props }) {
-    if (!isLoading) return <Component {...props} />;
+  return function SongsLoadingComponent({ isLoading, errorMessage, ...props }) {
+    if (!isLoading && !errorMessage) return <Component {...props} />;
+    if (errorMessage) return <Alert variant="danger">{errorMessage}</Alert>;
     return (
       <p style={{ textAlign: "center", fontSize: "30px" }}>
         Loading... Please Stand By.
