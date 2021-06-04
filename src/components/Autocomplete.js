b147d8ef6f86fba5e333ab826db7dcc442a18d87
Max Grayer <mgrayer@andplus.com>
2021-06-04 08:46:16 -0400

    Adds search component

---
+++ src/components/Autocomplete.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { CardDeck } from "react-bootstrap";
+import SongItem from "./SongItem";
+
+const Autocomplete = (props) => {
+  const items = props.songs;
+
+  const [state, setState] = useState({
+    activeItem: 0,
+    filteredItems: [],
+    displayItems: false,
+    inputValue: "",
+  });
+
+  const handleChange = (e) => {
+    const inputValue = e.currentTarget.value;
+    const filteredItems = items.filter(
+      (optionSong) =>
+        optionSong.artist.toLowerCase().indexOf(inputValue.toLowerCase()) >
+          -1 ||
+        optionSong.title.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
+    );
+
+    setState({
+      activeItem: 0,
+      filteredItems,
+      displayItems: true,
+      inputValue: e.currentTarget.value,
+    });
+  };
+
+  const handleClick = (e) => {
+    setState({
+      activeItem: 0,
+      filteredItems: [],
+      displayItems: false,
+      inputValue: e.currentTarget.innerText,
+    });
+  };
+
+  const handleKeyDown = (e) => {
+    const { activeItem, filteredItems } = state;
+
+    if (e.keyCode === 13) {
+      // keyCode 13 is the "enter" key
+      setState({
+        activeItem: 0,
+        filteredItems: [],
+        displayItems: false,
+        inputValue: filteredItems[activeItem],
+      });
+    } else if (e.keyCode === 38) {
+      // keyCode 38 is the up arrow key
+      e.preventDefault();
+      if (activeItem === 0) {
+        return;
+      }
+      setState({
+        activeItem: activeItem - 1,
+        filteredItems,
+        displayItems: true,
+        inputValue: e.currentTarget.value,
+      });
+    } else if (e.keyCode === 40) {
+      // keyCode 40 is the down arrow
+      e.preventDefault();
+      if (
+        (filteredItems && activeItem === filteredItems.length - 1) ||
+        activeItem >= 9
+      ) {
+        return;
+      }
+      setState({
+        activeItem: activeItem + 1,
+        filteredItems,
+        displayItems: true,
+        inputValue: e.currentTarget.value,
+      });
+    }
+  };
+
+  return (
+    <div className="search-field">
+      <input
+        name="songs"
+        placeholder="Enter an artist or title"
+        value={state.inputValue}
+        autoComplete="off"
+        autoCapitalize="off"
+        autoCorrect="off"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      {state.displayItems &&
+      state.inputValue.length &&
+      state.filteredItems.length ? (
+        <CardDeck>
+          {state.filteredItems
+            .map((optionSong) => {
+              return (
+                <SongItem
+                  key={optionSong.id}
+                  {...optionSong}
+                  onClick={handleClick}
+                />
+              );
+            })
+            .slice(0, 10)}
+        </CardDeck>
+      ) : null}
+    </div>
+  );
+};
+
+export default Autocomplete;
--- src/components/BareList.js
+++ src/components/BareList.js
@@ -1,17 +1,12 @@
 import React from "react";
-import CardDeck from "react-bootstrap/CardDeck";
-import SongItem from "./SongItem";
+import Autocomplete from "./Autocomplete";
 const BareList = (props) => {
   const { songs } = props;
   if (!songs || songs.length === 0) return <p>No songs, sorry!</p>;
   return (
     <div>
       <h2 className="list-head">Complete Song Library</h2>
-      <CardDeck>
-        {songs.map((song) => {
-          return <SongItem key={song.id} {...song} />;
-        })}
-      </CardDeck>
+      <Autocomplete songs={songs} />
     </div>
   );
 };
